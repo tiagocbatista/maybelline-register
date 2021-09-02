d46141be8536382d1a26711380fa1c1822e1fe3c
Tiago Batista <tiagocbatista@gmail.com>
2021-09-01 23:43:00 -0300

    big changes

--- controllers/clientController.js
+++ controllers/clientController.js
@@ -3,11 +3,19 @@ const Client = require('../models/Client');
 const addClient = async (req, res) => {
     let client = new Client(req.body);
 
-    try {
-        let doc = await client.save();
-        res.send('Cliente adicionada com sucesso');
-    } catch (error) {
-        res.send(error);
+    let doc = await Client.findOne({
+        cpf: client.cpf
+    });
+
+    if (!doc) {
+        try {
+            let doc = await client.save();
+            res.render('page-aeroporto-register', {registrationSuccess: true});
+        } catch (error) {
+            res.send(error);
+        }
+    } else {
+        res.send('CPF já cadastrado');
     }
 }
 
@@ -17,11 +25,31 @@ const searchClient = async (req, res) => {
             cpf: req.body.cpf
         });
 
-        if (doc == null) {
+        if (!doc) {
             res.send('Nenhuma cliente encontrada');
         } else {
-            res.send(doc);
+            res.render('update-client', {
+                body: doc
+            });
         }
+    } catch (error) {
+        res.send(error);
+    }
+}
+
+const updateClient = async (req, res) => {
+    try {
+        let doc = await Client.findOneAndUpdate({
+            cpf: req.body.cpf
+        }, {
+            $inc: {
+                compra: req.body.compra,
+                boletos: 1
+            },
+            bairro: req.body.bairro, telefone: req.body.telefone
+        });
+
+        res.send('Atualizado com sucesso');
     } catch (error) {
         res.send(error);
     }
@@ -29,5 +57,6 @@ const searchClient = async (req, res) => {
 
 module.exports = {
     addClient,
-    searchClient
+    searchClient,
+    updateClient
 }
--- routes/clientRoute.js
+++ routes/clientRoute.js
@@ -4,13 +4,22 @@ const clientController = require('../controllers/clientController');
 
 /* Definição de Rotas */
 
+router.get('/aeroporto', (req, res) => {
+    res.render('page-aeroporto-register', {registrationSuccess: false});
+});
+
+router.get('/updateClient', (req, res) => {
+    res.render('update-client');
+})
+
+
 router.post('/search', express.urlencoded({
     extended: true
 }), clientController.searchClient);
 
-router.get('/aeroporto', (req, res) => {
-    res.render('page-aeroporto-register');
-});
+router.post('/updateClient', express.urlencoded({
+    extended: true
+}), clientController.updateClient);
 
 router.post('/', express.urlencoded({
     extended: true
